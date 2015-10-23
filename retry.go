3f6c5c6f420bf2636b89e358aad8dc9abdc905f9
Tim Penhey <tim@penhey.net>
2015-10-23 16:58:15 +1300

    Move doc to its own file, and remove the scale function

--- retry.go
+++ retry.go
@@ -1,104 +1,10 @@
 // Copyright 2015 Canonical Ltd.
 // Licensed under the LGPLv3, see LICENCE file for details.
 
-// The retry package encapsulates the mechanism around retrying commands.
-//
-// The simple use is to call retry.Call with a function closure.
-//
-// ```go
-//	err := retry.Call(retry.CallArgs{
-//		Func:     func() error { ... },
-//		Attempts: 5,
-//		Delay:    time.Minute,
-//		Clock:    clock.WallClock,
-//	})
-// ```
-//
-// The bare minimum arguments that need to be specified are:
-// * Func - the function to call
-// * Attempts - the number of times to try Func before giving up
-// * Delay - how long to wait between each try that returns an error
-// * Clock - either the wall clock, or some testing clock
-//
-// Any error that is returned from the `Func` is considered transient.
-// In order to identify some errors as fatal, pass in a function for the
-// `IsFatalError` CallArgs value.
-//
-// In order to have the `Delay` change for each iteration, a `BackoffFunc`
-// needs to be set on the CallArgs. A simple doubling delay function is
-// provided by `DoubleDelay`.
-//
-// An example of a more complex `BackoffFunc` could be a stepped function such
-// as:
-//
-// ```go
-//	func StepDelay(last time.Duration, attempt int) time.Duration {
-//		switch attempt{
-//		case 1:
-//			return time.Second
-//		case 2:
-//			return 5 * time.Second
-//		case 3:
-//			return 20 * time.Second
-//		case 4:
-//			return time.Minute
-//		case 5:
-//			return 5 * time.Minute
-//		default:
-//			return 2 * last
-//		}
-//	}
-// ```
-//
-// Consider some package `foo` that has a `TryAgainError`, which looks something
-// like this:
-// ```go
-//	type TryAgainError struct {
-//		After time.Duration
-//	}
-// ```
-// and we create something that looks like this:
-//
-// ```go
-//	type TryAgainHelper struct {
-//		next time.Duration
-//	}
-//
-//	func (h *TryAgainHelper) notify(lastError error, attempt int) {
-//		if tryAgain, ok := lastError.(*foo.TryAgainError); ok {
-//			h.next = tryAgain.After
-//		} else {
-//			h.next = 0
-//		}
-//	}
-//
-//	func (h *TryAgainHelper) next(last time.Duration) time.Duration {
-//		if h.next != 0 {
-//			return h.next
-//		}
-//		return last
-//	}
-// ```
-//
-// Then we could do this:
-// ```go
-//	helper := TryAgainHelper{}
-//	retry.Call(retry.CallArgs{
-//		Func: func() error {
-//			return foo.SomeFunc()
-//		},
-//		NotifyFunc:  helper.notify,
-//		BackoffFunc: helper.next,
-//		Attempts:    20,
-//		Delay:       100 * time.Millisecond,
-//		Clock:       clock.WallClock,
-//	})
-// ```
 package retry
 
 import (
 	"fmt"
-	"math"
 	"time"
 
 	"github.com/juju/errors"
@@ -256,21 +162,4 @@ func DoubleDelay(delay time.Duration, attempt int) time.Duration {
 		return delay
 	}
 	return delay * 2
-}
-
-// ScaleDuration scale up the `current` duration by a factor of `scale`, with
-// a capped value of `max`. If `max` is zero, it means there is no maximum
-// duration.
-func ScaleDuration(current, max time.Duration, scale float64) time.Duration {
-	// Any overhead that we may possibly incur by multiplying something by one,
-	// is more than overcome by the sleeping that will occur.
-
-	// Since scale may be something like 1.5, or 2 and time.Duration is an
-	// int64, we need a little casting here. Also, a negative scale is treated
-	// as positive.
-	duration := (time.Duration)((float64)(current) * math.Abs(scale))
-	if duration > max && max > 0 {
-		return max
-	}
-	return duration
 }
